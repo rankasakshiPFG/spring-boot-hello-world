5ccf14227f0e53aa714fb60a1466d9942b27d5f9
rankasakshiPFG <83642909+rankasakshiPFG@users.noreply.github.com>
2021-12-20 17:49:49 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/example/hello/HelloWorldApplication.java
+++ src/main/java/com/example/hello/HelloWorldApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorldApplication {
 
-	public static void main(String[] args) {
+	public static void main(Stri args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 }
